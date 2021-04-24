f00e76a5839cf61139b3be2ca9d4cb60300d663a
PantsuDango <394883561@qq.com>
2021-04-24 12:08:42 +0800

    改app名字

--- git.sh
+++ git.sh
@@ -5,7 +5,7 @@ path=$PWD
 cd $path/service
 num=$(netstat -nlpt | grep ":::5000" | grep -v "grep" | wc -l)
 if [ $num -eq 1 ]; then
-    kill -9 $(ps -ef | grep "app" | grep -v "grep" | awk '{print $2}')
+    kill -9 $(ps -ef | grep "social" | grep -v "grep" | awk '{print $2}')
     sleep 3
 fi
 nohup go run app.go &
@@ -13,9 +13,9 @@ sleep 3
 
 num=$(netstat -nlpt | grep ":::5000" | grep -v "grep" | wc -l)
 if [ $num -eq 1 ]; then
-    echo "run app success"
+    echo "run social success"
 else
     cd $path/app
     tail -n 10 nohup.out
-    echo "run app fail"
+    echo "run social fail"
 fi
