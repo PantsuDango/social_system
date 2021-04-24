1ee6841f7612df023772630676c8bd80283470df
PantsuDango <394883561@qq.com>
2021-04-24 12:13:47 +0800

    改app名字

--- git.sh
+++ git.sh
@@ -8,7 +8,7 @@ if [ $num -eq 1 ]; then
     kill -9 $(ps -ef | grep "social" | grep -v "grep" | awk '{print $2}')
     sleep 3
 fi
-nohup go run app.go &
+nohup go run social.go &
 sleep 3
 
 num=$(netstat -nlpt | grep ":::5000" | grep -v "grep" | wc -l)
